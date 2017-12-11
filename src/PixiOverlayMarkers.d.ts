3a70b2abcba2a7d14b7829c249ddf0dc84b936ef
Steve Gentile <steve.gentile@appliedis.com>
2017-12-11 13:23:25 -0500

    wip

--- src/PixiOverlayMarkers.d.ts
+++
@@ -1,8 +0,0 @@
-export default class PixiOverlayMarkers {
-    constructor(map: any, resources: any, options: any);
-    createLayer(id: any, data: any, createShapeCallback: any): any;
-    removeLayer(id: any): void;
-    hasLayer(id: any): any;
-    render(): void;
-    convertColorToHex(color: any): number;
-}
